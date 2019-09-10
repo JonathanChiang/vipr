e05eff0dcead67fcafb5bc9479df05896b75fb7e
Jonathan Chiang <13003102+JonathanChiang@users.noreply.github.com>
2019-09-10 16:21:31 -0700

    Update rbind.py

--- utilities/rbind.py
+++ utilities/rbind.py
@@ -1,1 +1,13 @@
 
+import pandas as pd
+
+'''
+# https://stackoverflow.com/questions/14988480/pandas-version-of-rbind
+
+df1 = pd.DataFrame({'col1': [1,2], 'col2':[3,4]})
+df2 = pd.DataFrame({'col1': [5,6], 'col2':[7,8]})
+print(df1)
+print(df2)
+print(pd.concat([df1, df2]))
+'''
+
