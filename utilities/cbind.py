10353cf7c8e53c1fcc474de0a6a2942f0aa7ec07
Jonathan Chiang <13003102+JonathanChiang@users.noreply.github.com>
2019-09-10 16:46:28 -0700

    Update cbind.py

--- utilities/cbind.py
+++ utilities/cbind.py
@@ -1,14 +1,18 @@
 '''
 [stackoverflow](https://stackoverflow.com/questions/28595701/pandas-equivalent-of-rs-cbind-concatenate-stack-vectors-vertically)
 import pandas
-....
-....
-test1 = pandas.DataFrame([1,2,3,4,5])
-....
-....
-test2 = pandas.DataFrame([4,2,1,3,7])
-....
 
+test1 = pandas.DataFrame([1,2,3,4,5])
+test2 = pandas.DataFrame([4,2,1,3,7])
+
+Should end up like this: 
+cbindR(test1,test2)
+   v0  v1
+0   1   5
+1   2   4
+2   3   3
+3   4   2
+4   5   1
 
 '''
 
