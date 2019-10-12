06515de859aad38049d5468e7e8e81e78e49741b
Gordon Fleetwood <gfleetwood@protonmail.com>
2019-10-11 20:52:05 -0400

    Rewrote rbind to take an arbitrary number of dataframes. Added examples to both rbind & cbindR.

--- utilities/cbind.py
+++ utilities/cbind.py
@@ -1,5 +1,9 @@
 '''
 [stackoverflow](https://stackoverflow.com/questions/28595701/pandas-equivalent-of-rs-cbind-concatenate-stack-vectors-vertically)
+
+Example 1
+---------
+
 import pandas
 
 test1 = pandas.DataFrame([1,2,3,4,5])
@@ -14,6 +18,18 @@ cbindR(test1,test2)
 3   4   2
 4   5   1
 
+Example 2
+---------
+
+import numpy as np
+import pandas as pd
+from sklearn import datasets
+
+iris = datasets.load_iris()
+df = pd.DataFrame(iris.data[:, :], columns = ["f{}".format(i) for i in range(1,5)])  
+dfs = [df for i in range(10)]
+
+cbindR(dfs)
 '''
 
 def cbindR(*dfs):
--- utilities/rbind.py
+++ utilities/rbind.py
@@ -1,6 +1,9 @@
 '''
 # https://stackoverflow.com/questions/14988480/pandas-version-of-rbind
 
+Example 1
+---------
+
 df1 = pd.DataFrame({'col1': [1,2], 'col2':[3,4]})
 df2 = pd.DataFrame({'col1': [5,6], 'col2':[7,8]})
 print(df1)
@@ -12,9 +15,29 @@ should be able to bind multiple at once
 
 pd.concat([df1, df2]) == rbind(df1, df2) 
 
+Example 2
+---------
+
+import numpy as np
+import pandas as pd
+from sklearn import datasets
+
+iris = datasets.load_iris()
+df = pd.DataFrame(iris.data[:, :], columns = ["f{}".format(i) for i in range(1,5)])  
+dfs = [df for i in range(10)]
+
+rbind(dfs)
+
 '''
 
+import numpy as pd
 import pandas as pd
-def rbind(df1, df2):
-  return pd.concat([df1,df2])
+
+def rbind(*dfs):
+    
+    # Add assertion for all sets of columns being equal
+
+    result = pd.concat(dfs[0], axis = 0, ignore_index = True)
+    
+    return result
  
